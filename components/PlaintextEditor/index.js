ff494daa1af26817c4ac9b261b618bfee3ee614e
Clark Perfecto <a1clark1a@gmail.com>
2020-10-20 21:19:11 -0700

    plaintextEditor

--- components/PlaintextEditor/index.js
+++ components/PlaintextEditor/index.js
@@ -1,15 +1,74 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
-import css from './style.css';
+import path from 'path';
+import css from '../../styles/editor.module.css';
 
 function PlaintextEditor({ file, write }) {
-  console.log(file, write);
+  const [newFile, setNewFile] = useState({});
+  const [allowEdit, setAllowEdit] = useState(false);
+
+  //console.log(file, write);
+  useEffect(() => {
+    (async () => {
+      setNewFile({
+        text: await file.text(),
+        name: await path.basename(file.name)
+      });
+    })();
+  }, [file]);
+
+  const save = e => {
+    e.preventDefault();
+    const file = new File([newFile.text], newFile.name, {
+      type: 'text/plain',
+      lastModified: new Date()
+    });
+    write(file);
+  };
+
+  const handleChange = event => {
+    setNewFile({ ...newFile, text: event.target.value });
+  };
+
   return (
-    <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/plain</i>
-    </div>
+    <form className={css.editor} onSubmit={save}>
+      {allowEdit ? (
+        <div>
+          <input
+            type="text"
+            onChange={e => setNewFile({ ...newFile, name: e.target.value })}
+          ></input>
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => setAllowEdit(false)}
+          >
+            Save
+          </button>
+        </div>
+      ) : (
+        <h3 className="name">
+          {file && newFile.name ? newFile.name : 'add a name for the file'}
+
+          <button
+            className={css.button}
+            type="button"
+            onClick={() => setAllowEdit(true)}
+          >
+            Edit
+          </button>
+        </h3>
+      )}
+      <textarea
+        value={file && newFile.text ? newFile.text : 'Write your text here'}
+        rows="30"
+        cols="70"
+        onChange={handleChange}
+      ></textarea>
+      <button className={css.button} type="submit">
+        Save
+      </button>
+    </form>
   );
 }
 
